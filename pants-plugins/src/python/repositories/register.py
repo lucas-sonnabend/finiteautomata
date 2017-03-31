db2a4ae749ccf1ab9963fbd2e4bb4fc214420dce
Lucas Sonnabend <lucas.sonnabend@cantab.net>
2017-03-31 10:57:17 +0100

    add functionality to publish locally by adding a fake public repo

--- pants-plugins/src/python/repositories/register.py
+++ pants-plugins/src/python/repositories/register.py
@@ -1,8 +1,16 @@
-from packend.jvm.repository import Repository
+from pants.backend.jvm.repository import Repository
+from pants.build_graph.build_file_aliases import BuildFileAliases
+
 import os
 
 public_repo = Repository(
 	name = "public",
 	url = "TODO",
-	push_db_basedir = os.path.join("build-support", "ivy", "pushdb")
-)
+	push_db_basedir = os.path.join("build-support", "ivy", "pushdb"))
+
+def build_file_aliases():
+	return BuildFileAliases(
+		objects={
+			'public': public_repo,
+		  },
+	)
